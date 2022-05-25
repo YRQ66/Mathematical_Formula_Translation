31b59f68f9397630691f3492c3b3559c36aca89a
kwakjeeyoon <jeeyoon744@naver.com>
2022-05-26 00:46:48 +0900

    [FIX] alphabet error fixed

--- crawler.py
+++ crawler.py
@@ -1,3 +1,4 @@
+import re
 import json
 import requests
 from tqdm import tqdm
@@ -11,7 +12,7 @@ def crawling(file_path, url):
     #root > div.Content__Background-sc-1f451g1-0.gEoQaE > div.Content__OuterWrapper-sc-1f451g1-5.eEKyye > div > div > div.ContentPane__Wrapper-sc-6et83r-0.kpTBXq > div > div > div > div.RedoPadding-sc-1rj2rww-0.gaOlpC > div.page-content.PageContent-ny9bj0-0.cXwdbF
 
     data = {}
-    data['statistics'] = [] # 범용화 필요
+    data['statistics'] = [] # 범용화 필요 -> url에서 book 다음에 나오는 이름으로 지정 [변경 팔요]
 
     base = url.split('/')
     base = '/'.join(base[:-1])+'/'
@@ -24,18 +25,20 @@ def crawling(file_path, url):
                 title = title.get_text()
             # url 당 content 가져오기
             url = urljoin(base, link.get('href'))  # base 링크와 상대경로 링크를 결합
-            soup = bs(requests.get(url).text, 'html.parser')
+            soup = bs(requests.get(url).content, 'html.parser', from_encoding='utf-8')
             content = soup.find('div', id="main-content") # main content 가져오기
 
             # ('div.main-content > div.chapter-content-module > div.unnumbered (여기에 mathml 코드 있음... 왜그렇지?) > div.MathJax_Display > span.MathJax')
             mathjax = content.select('div.chapter-content-module > div.unnumbered')
             if mathjax:
-                math = str(mathjax[0])
-                math = math.split('>')
-                mathml = ">".join(math[1:-1])
+                mathml_all = []
+                for mathml in mathjax:
+                    mathml = str(mathml).split('>')[1:-2]
+                    mathml = ">".join(mathml) + '>'
+                    mathml_all.append(mathml)
                 data['statistics'].append({"chapter" : number,
                                            "title" : title,
-                                           "mathml" : mathml,
+                                           "mathml" : mathml_all,
                                            "content" : content.get_text()})
     with open(file_path, 'w') as file:
         json.dump(data, file)

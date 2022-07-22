fe86202e46cdc9fce2b1c04bfbf049ab793e3b69
kwakjeeyoon <jeeyoon744@naver.com>
2022-07-22 21:33:34 +0900

    [refactor]dataset structure change

--- im2latex/tokenizer.py
+++ im2latex/tokenizer.py
@@ -1,4 +1,6 @@
+import pandas as pd
 from os.path import join
+from this import d
 from preprocess import preprocess_df
 
 # Create and train word level tokenizer
@@ -8,7 +10,9 @@ from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.processors import TemplateProcessing
 
-def tokenizer(formulas_file, data_dir, max_length = None, vocab_size = None):
+def tokenizer(formulas_file, data_dir, max_length = None, vocab_size = None, dataset_type = '140K'):
+
+    root_dir = join(data_dir, dataset_type)
 
     tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
     tokenizer.enable_padding(length=max_length)
@@ -30,7 +34,7 @@ def tokenizer(formulas_file, data_dir, max_length = None, vocab_size = None):
 
     files = [formulas_file]
     tokenizer.train(files, trainer)
-    tokenizer.save(join(data_dir, "tokenizer-wordlevel.json"))
+    tokenizer.save(join(root_dir, "tokenizer-wordlevel.json"))
     return tokenizer
 
 if __name__ == '__main__':
@@ -40,9 +44,12 @@ if __name__ == '__main__':
     # tokenize
     parser.add_argument("-m", "--max_length_token", default=100)
     parser.add_argument("-v", "--vocab_size", default=600)
+    parser.add_argument("-t", "--dataset_type", default='140K')
     args = parser.parse_args()
 
-    formulas_file = join(args.data_dir, "im2latex_formulas_utf.lst")
+    dataset_dir = join(args.data_dir, args.dataset_type)
+    formulas_file = join(dataset_dir, "formulas.txt")
+    # print(formulas_file)
     # linux 인코딩 변환 : iconv -c -f ISO-8859-1 -t utf-8 im2latex_formulas.lst > im2latex_formulas_utf.lst
     with open(formulas_file, 'r') as f:
         formulas = [formula.strip('\n') for formula in f.readlines()]
@@ -50,7 +57,8 @@ if __name__ == '__main__':
     # train_df/test_df/validate_df
     types = ['train', 'valid', 'test']
     for type in types:
-        df = preprocess_df(formulas, data_dir = args.data_dir, type = type, max_length_token=args.max_length_token)
+        df_dir = '{}.pkl'.format(type)
+        df = pd.read_pickle(join(dataset_dir, df_dir))
         globals()["{}_df".format(type)] = df
 
     tokenizer = tokenizer(formulas_file = formulas_file, data_dir = args.data_dir, max_length = args.max_length_token, vocab_size=args.vocab_size)
@@ -58,6 +66,6 @@ if __name__ == '__main__':
     # Sanity check of tokenizer
     i = 5
 
-    print(tokenizer.encode(train_df.loc[i, 'text']).tokens )
-    print(tokenizer.encode(train_df.loc[i, 'text']).ids )
+    print(tokenizer.encode(train_df.loc[i, 'formula']).tokens )
+    print(tokenizer.encode(train_df.loc[i, 'formula']).ids )
     print(tokenizer.token_to_id("[PAD]") )

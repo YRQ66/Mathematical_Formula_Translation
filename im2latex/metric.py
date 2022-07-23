a674121f626c33ea2ed0bb6ea9775564a591e185
jiwoo <ann9902@gmail.com>
2022-07-23 13:43:02 +0900

    [chore] del print

--- im2latex/metric.py
+++ im2latex/metric.py
@@ -16,16 +16,7 @@ def compute_cer(pred_ids, label_ids, tokenizer):
     return cer
 
 def get_pred_and_label_str(pred, label, tokenizer):
-    # print(f'pred shape : {pred.shape}')
-    # print(f'pred : {pred}')
     pred_str = tokenizer.decode_batch(pred.tolist(), skip_special_tokens=True)
-    print(f'pred_str : {pred_str}')
-    
-    # print(f'label shape : {label.shape}')
-    # print(f'label: {label}')
     label[label == -100] = tokenizer.token_to_id("[PAD]")
-    # print(f'modified label shape : {label.shape}')
-    # print(f'modified label : {label}')
     label_str = tokenizer.decode_batch(label.tolist(), skip_special_tokens=True)
-    print(f'label_str : {label_str}')
     return (pred_str, label_str)

ebf28eb02a360b1523efb0a4cf65b278d4b7fbd7
kwakjeeyoon <jeeyoon744@naver.com>
2022-07-25 19:54:50 +0900

    [add]rendering formula to image

---
+++ im2latex/create_dataset/formula_to_image.py
@@ -0,0 +1,71 @@
+import os
+from os.path import join
+import hashlib
+import subprocess
+from threading import Timer
+from tqdm import tqdm
+
+from latex_crawler import save_latex
+
+DEVNULL = open(os.devnull, "w")
+
+def run(cmd, timeout_sec):
+    """Run cmd in the shell with timeout"""
+    proc = subprocess.Popen(cmd, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+    kill_proc = lambda p: p.kill()
+    timer = Timer(timeout_sec, kill_proc, [proc])
+    try:
+        timer.start()
+        stdout,stderr = proc.communicate()
+    finally:
+        timer.cancel()
+
+def formula_to_image(formula, data_path = './'):
+    formula = formula.strip("%")
+    name = hashlib.sha1(formula.encode('utf-8')).hexdigest()[:15]
+
+    # write formula into a .tex file
+    tex_output = join(data_path, 'pdf')
+    with open(tex_output + "/{}.tex".format(name), "w") as f:
+        f.write(
+        r"""\documentclass[preview]{standalone}
+        \begin{document}
+            $$ %s $$
+        \end{document}""" % (formula))
+    
+    # call pdflatex to create pdf
+    run("pdflatex -interaction=nonstopmode -output-directory={} {}".format(
+        tex_output, tex_output+"/{}.tex".format(name)), 10)
+    # call(['pdflatex', '-interaction=nonstopmode','-halt-on-error'])
+
+    img_output = join(data_path, 'images')
+    # call magick to convert the pdf into a png file
+    run("magick convert -density {} -quality {} {} {}".format(200, 100,
+        tex_output+"/{}.pdf".format(name), img_output+"/{}.png".format(name)),
+        10)
+
+def make_folder(path):
+    folder_li = ['formulas', 'pdf', 'images']
+    for folder in folder_li:
+        if not os.path.isdir(join(path, folder)):
+            os.mkdir(folder)
+
+
+if __name__=='__main__':
+    data_path = './'
+    make_folder(path = data_path)
+    # crawling 
+    url = "https://ko.wikipedia.org/wiki/%ED%86%B5%EA%B3%84%ED%95%99" # 위키백과 통계학 페이지
+    # save_latex(url)
+
+    formula_path = join(data_path,'formulas')
+    formula_li = os.listdir(formula_path)
+    # 일단 통계학.lst 로 테스트 -> for문으로 바꾸기 (+렌더링한 txt 파일은 다시 하지 않도록 조건문 넣기)
+    print('Start rendering {} files ....'.format(len(formula_li)))
+    for i in range(len(formula_li)):
+        if formula_li[i].endswith('.txt'):
+            with open(join(formula_path, formula_li[i]), 'r') as f:
+                formulas = [formula.strip('\n') for formula in f.readlines()]
+                for formula in tqdm(formulas):
+                    formula_to_image(formula, data_path = data_path)
+    
--- im2latex/create_dataset/latex_crawler.py
+++ im2latex/create_dataset/latex_crawler.py
@@ -8,14 +8,14 @@ def get_url(url):
     # origin url에 포함된 모든 링크 크롤링
     req = requests.get(url)
     soup = BeautifulSoup(req.content, "html.parser")
-    url_li = []
+    url_li = set()
     for line in soup.select("body"):
         content_name = line.find('h1').text # 각 데이터베이스마다 카테고리를 명시하기 위해 (아직 코드에 반영 안됨, ex. 통계학)
         can_url = line.find_all('a')
         for can_u in can_url:
             url = can_u.get('href')
             if url!=None and url.startswith('https://'):
-                url_li.append(url)
+                url_li.add(url)
     return url_li
     
 def get_latex(url, latex_li = []):
@@ -33,7 +33,7 @@ def get_latex(url, latex_li = []):
                 latex_li.append(latex)
     return latex_li
 
-def save_latex(origin_url, save_dir = './formula'):
+def save_latex(origin_url, save_dir = './formulas'):
     # crawl latex
     print('Crwaling LaTeX data....')
     latex = set() # 중복 제거
@@ -41,15 +41,16 @@ def save_latex(origin_url, save_dir = './formula'):
     url_li.extend(get_url(origin_url)) # url crawling
     for url in tqdm(url_li):
         latex.update(get_latex(url)) # latex crawling
-    print('Crawling Done!')
+    print('Crawling Done! Total {}'.format(len(latex)))
     str_latex = '\n'.join(latex)
     # save
-    data_name = "{}.lst".format(origin_url.split('/')[-1])
+    data_name = "{}.txt".format(origin_url.split('/')[-1])
     full_dir = join(save_dir, data_name)
     with open(full_dir, "w") as f:
         f.write(str_latex)
     
 
 if __name__=='__main__':
+    # 통계학 페이지 : 118개
     url = "https://ko.wikipedia.org/wiki/%ED%86%B5%EA%B3%84%ED%95%99"
     save_latex(url)

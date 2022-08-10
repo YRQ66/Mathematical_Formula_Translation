322ddbc5899a2528eecc3963f4543e8ead81a2b3
jiwoo <ann9902@gmail.com>
2022-08-11 00:31:37 +0900

    [chore] remove .png

--- im2latex/dataset.py
+++ im2latex/dataset.py
@@ -33,7 +33,7 @@ class IAMDataset(Dataset):
         #     idx = idx - 1 if idx > 0 else idx + 1 
         #     return self.__getitem__(idx)
 
-        image = Image.open(join(self.root_dir, file_name+'.png')).convert("RGB")
+        image = Image.open(join(self.root_dir, file_name)).convert("RGB")
         pixel_values = self.processor(image, return_tensors="pt").pixel_values
 #        # add labels (input_ids) by encoding the text
 #        labels = self.processor.tokenizer(text, 
